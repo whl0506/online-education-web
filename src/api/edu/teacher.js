50dca06233d3028e91e2b33909f0bbf3954a1cdd
whl <whl0506@163.com>
2020-11-27 12:11:10 +0800

    在线教育项目前端初始化

---
+++ src/api/edu/teacher.js
@@ -0,0 +1,39 @@
+import request from '@/utils/request'
+
+const api_name = '/admin/edu/teacher'
+export default {
+
+  getPageList(page, limit, searchObj) {
+    return request({
+      url: `${api_name}/page/${page}/${limit}`,
+      method: 'get',
+      data: searchObj
+    })
+  },
+  removeById(teacherId) {
+    return request({
+      url: `${api_name}/delete/${teacherId}`,
+      method: 'delete'
+    })
+  },
+  save(teacher) {
+    return request({
+      url: `${api_name}/add_or_update`,
+      method: 'post',
+      data: teacher
+    })
+  },
+  getById(id) {
+    return request({
+      url: `${api_name}/${id}`,
+      method: 'get'
+    })
+  },
+  updateById(teacher) {
+    return request({
+      url: `${api_name}/add_or_update`,
+      method: 'post',
+      data: teacher
+    })
+  }
+}
